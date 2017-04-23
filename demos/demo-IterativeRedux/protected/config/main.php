e1d75a5621297c5c768440e02b2a6618e73ad7a4
Jovi <redzjovi@gmail.com>
2017-04-23 18:31:23 +0700

    Update config, create model using gii.

--- demos/demo-IterativeRedux/protected/config/main.php
+++ demos/demo-IterativeRedux/protected/config/main.php
@@ -20,14 +20,13 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'gii',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -39,7 +38,6 @@ return array(
 		),
 
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -48,8 +46,7 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-
+		
 		// database settings are configured in database.php
 		'db'=>require(dirname(__FILE__).'/database.php'),
 
