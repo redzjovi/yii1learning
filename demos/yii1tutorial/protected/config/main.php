5578f6fdaab2c14a6e0a4a81c9eb310b928a71c8
Jovi <redzjovi@gmail.com>
2017-05-01 22:42:16 +0700

    Yii remove / hide index.php.

--- demos/yii1tutorial/protected/config/main.php
+++ demos/yii1tutorial/protected/config/main.php
@@ -45,6 +45,7 @@ return array(
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
+            'showScriptName' => false,
 		),
 		
 		// database settings are configured in database.php
